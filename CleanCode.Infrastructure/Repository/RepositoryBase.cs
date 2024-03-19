2ae3b5933c680496af6d13fa975dd2548fe5d0eb
AlyanKhan <alyan.khan@decibelbpo.com>
2024-03-19 15:17:13 +0500

    modified

--- CleanCode.Infrastructure/Repository/RepositoryBase.cs
+++ CleanCode.Infrastructure/Repository/RepositoryBase.cs
@@ -1,5 +1,7 @@
 ﻿using Amazon.Runtime.Internal;
+using CleanCode.Domain.Database.MongoInterface;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,14 @@ namespace CleanCode.Infrastructure.Repository
 {
     public class RepositoryBase
     {
-        protected RepositoryBase(IConfiguration configuration) 
+        private readonly IConfiguration configuration;
+        private readonly string? databaseName;
+        private readonly MongoClient mongoClient;
+        protected RepositoryBase(IConfiguration configuration, IMongoClientFactory mongoClientFactory) 
         {
-
+            this.configuration = configuration;
+            this.databaseName = configuration.GetConnectionString("Mongo:DatabaseName");
+            this.mongoClient = mongoClientFactory.GetMongoClient();
         }
     }
 }
