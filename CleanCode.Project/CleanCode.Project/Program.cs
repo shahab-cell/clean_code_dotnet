f27b8eca13d6e99cab36ad81a79e549fcdc07297
AlyanKhan <alyan.khan@decibelbpo.com>
2024-04-15 15:52:54 +0500

    dependency injected for Product classes

--- CleanCode.Project/CleanCode.Project/Program.cs
+++ CleanCode.Project/CleanCode.Project/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddScoped<IMongoClientFactory, MongoClientFactory>();
 builder.Services.AddScoped<IPasswordCoder, PasswordCoder>();
 builder.Services.AddScoped<IUserApplication, UserApplication>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProductApplication, ProductApplication>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
